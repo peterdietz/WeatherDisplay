e77091a696ae3d91b531e98f0540f9137b1eceb9
Peter Dietz <pdietz84@gmail.com>
2012-02-09 09:13:56 -0500

    Using XPATH to fetch piece of data needed from XML feed.
    Need to figure out how to get multiple pieces of information from XPATH,
    because calling another xpath expression will fail due to end of connection.
    Will look into geting the NodeSet / NodeList working.

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -1,9 +1,18 @@
 package com.example;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,19 +28,54 @@ public class MyActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         StringBuilder builder = new StringBuilder();
-        builder.append("Before query");
         try {
             URL weatherURL = new URL("http://www.google.com/ig/api?weather=43202");
             URLConnection connection = weatherURL.openConnection();
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            InputSource inputSource = new InputSource(connection.getInputStream());
 
-            while((inputLine = br.readLine()) != null ) {
-                builder.append(inputLine);
-            }
-            br.close();
 
+            //NodeList nodes = (NodeList) xpath.evaluate("//current_conditions", inputSource, XPathConstants.NODESET);
+
+            //for(int i=0;i<nodes.getLength();i++)
+            //{
+            //    builder.append(xpath.evaluate("@data", nodes.item(i)));
+            //}
+
+
+            //String currentInfo = xpath.evaluate("/forecast_information/city/@data", nodes);
+
+
+            //builder.append("here");
+            //builder.append("Weather forecast for " + currentInfo);
+            //        + xpath.evaluate("//forecast_information/city/@data", inputSource) + "\n");
+
+            //connection = weatherURL.openConnection();
+            //inputSource = new InputSource(connection.getInputStream());
+            //xpath=XPathFactory.newInstance().newXPath();
+            //builder.append("Last Updated: "
+            //        + xpath.evaluate("//forecast_information/current_date_time/@data", inputSource) + "\n");
+            
+            //builder.append("Current Condition: "
+            //        + xpath.evaluate("//current_conditions/condition/@data", inputSource) + "\n");
+
+            builder.append("Temperature "
+                    + xpath.evaluate("//current_conditions/temp_f/@data", inputSource) + "\n");
+            builder.append("Humidity "
+                    + xpath.evaluate("//current_conditions/humidity/@data", inputSource) + "\n");
+            builder.append("Wind "
+                    + xpath.evaluate("//current_conditions/wind_condition/@data", inputSource) + "\n");
+            builder.append("Humidity "
+                    + xpath.evaluate("//current_conditions/humidity/@data", inputSource) + "\n");
+
+
+
+
+            /*ImageView imageView = new ImageView(this);
+            Uri imageURI = Uri.parse("http://www.google.com/" + xpath.evaluate("//current_conditions/icon/@data", inputSource));
+            imageView.setImageURI(imageURI);
+            setContentView(imageView);*/
 
 
         } catch (MalformedURLException e) {
@@ -40,14 +84,18 @@ public class MyActivity extends Activity
         } catch (IOException e) {
             builder.append("IO exception"+ e.getMessage());
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (XPathExpressionException e) {
+            builder.append("XPATH"+ e.getMessage());
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        builder.append("After query");
 
         TextView tv = new TextView(this);
-        tv.setText(builder);
+        tv.setText("before"+builder+"after");
+        setContentView(tv);
+        
         //
         // tv.setText("Hello, Android");
-        setContentView(tv);
+        
         //setContentView(R.layout.main);
     }
 }
